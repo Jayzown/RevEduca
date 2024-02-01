8fcacdd194759102b19d13328b85d76fae5e4336
Jayzown <aekpewoh@gmail.com>
2024-02-01 18:27:28 -0500

    Client and Server files added

---
+++ RevEduca-Server/src/main/java/com/educa/reveduca/RevEducaApplication.java
@@ -0,0 +1,13 @@
+package com.educa.reveduca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevEducaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevEducaApplication.class, args);
+	}
+
+}
---
+++ RevEduca-Server/src/test/java/com/educa/reveduca/RevEducaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educa.reveduca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevEducaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
